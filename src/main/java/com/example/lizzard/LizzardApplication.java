959079d1359cc69916b99d3d5fef018b1be4a107
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-04-08 21:16:52 -0500

    Initial commit

---
+++ src/main/java/com/example/lizzard/LizzardApplication.java
@@ -0,0 +1,13 @@
+package com.example.lizzard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LizzardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LizzardApplication.class, args);
+    }
+
+}
